814a83d88761e371ca83ac35bcaf16c2652041fc
Chuanqi Sun <chuanqisun-no-reply@gmail.com>
2025-07-31 23:50:10 -0700

    handle object key change

--- src/sdk/create-component.ts
+++ src/sdk/create-component.ts
@@ -7,7 +7,7 @@ export function createComponent<TProps extends object | undefined = undefined>(
   factory: (params: TProps) => Observable<TemplateResult> | TemplateResult,
 ) {
   return (params?: TProps) => {
-    const deps = params ? Object.values(params) : [];
+    const deps = params ? Object.entries(params).flatMap(([k, v]) => [k, v]) : [];
     return html`${guard(deps, () => {
       const result = factory(params as TProps);
       const template = isObservable(result) ? observe(result) : result;
